f599d27d855fbc98e398bdc9e7e0a11dc0245164
dmptmkn <dm.ptmkn@yandex.ru>
2024-07-12 15:04:09 +0300

    basic implementation of Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import printer.CoursePrinter;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CoursePrinter coursePrinter = new CoursePrinter();
+        coursePrinter.init();
+        coursePrinter.printData();
+    }
+}
