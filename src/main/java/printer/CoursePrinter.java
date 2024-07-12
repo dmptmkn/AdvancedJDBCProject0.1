979638036eb2e35a604bc4867b7fcc9c3bd9f27f
dmptmkn <dm.ptmkn@yandex.ru>
2024-07-12 23:48:47 +0300

    add basic CoursePrinter implementation

---
+++ src/main/java/printer/CoursePrinter.java
@@ -0,0 +1,69 @@
+package printer;
+
+import bean.Course;
+import bean.CourseType;
+
+import java.io.PrintStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoursePrinter {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/top";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "root";
+
+    private PrintStream printer;
+    private Connection connection;
+    private List<Course> courses;
+
+    public CoursePrinter() {
+    }
+
+    public void init() {
+        courses = new ArrayList<>();
+        printer = System.out;
+        getConnection();
+        collectData();
+    }
+
+    public void printData() {
+        printer.println("Вывожу данные в консоль:");
+        courses.forEach(printer::println);
+    }
+
+    private void getConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            printer.println("Соединение с базой данных установлено!");
+        } catch (SQLException e) {
+            System.out.println("Соединение с базой данных не установлено!");
+        }
+    }
+
+    private void collectData() {
+        String sqlQuery = "SELECT c.id, c.name, c.duration, c.type, c.description, t.name AS teacher_name, c.students_count, c.price, c.price_per_hour FROM courses AS c JOIN teachers AS t ON t.id = c.teacher_id";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Course nextCourse = Course.builder()
+                        .id(resultSet.getInt("id"))
+                        .name(resultSet.getString("name"))
+                        .duration(resultSet.getInt("duration"))
+                        .type(CourseType.valueOf(resultSet.getString("type")))
+                        .description(resultSet.getString("description"))
+                        .teacherName(resultSet.getString("teacher_name"))
+                        .studentsCount(resultSet.getInt("students_count"))
+                        .price(resultSet.getInt("price"))
+                        .pricePerHour(resultSet.getDouble("price_per_hour"))
+                        .build();
+                courses.add(nextCourse);
+            }
+        } catch (SQLException e) {
+            printer.println("Ошибка при работе с базой данных!");
+        }
+        printer.println("Данные собраны!");
+    }
+}
