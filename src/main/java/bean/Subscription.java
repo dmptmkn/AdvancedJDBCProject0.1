f7b9f54597842807cc5acc0a4b566208f6fd43bd
dmptmkn <dm.ptmkn@yandex.ru>
2024-07-15 15:32:09 +0300

    add Subscription class

---
+++ src/main/java/bean/Subscription.java
@@ -0,0 +1,16 @@
+package bean;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class Subscription {
+
+    private String studentName;
+    private String courseName;
+    private LocalDate subscriptionDate;
+
+}
